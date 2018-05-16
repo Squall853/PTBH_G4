6ee6d9e0cafa475c4924d3c0a7698a0722c8e4c9
Boi <squall89333@gmail.com>
2018-05-16 15:42:30 +0700

    Initial Commit

---
+++ PTBH_G4/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PTBH_G4
+//
+//  Created by Boi on 5/16/18.
+//  Copyright © 2018 Boi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
